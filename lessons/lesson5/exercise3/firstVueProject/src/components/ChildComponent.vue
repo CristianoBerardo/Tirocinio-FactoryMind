e51ed86661bb3b219ad163b0cd68003e56ddb8e3
Cristiano Berardo <75799096+CristianoBerardo@users.noreply.github.com>
2025-04-01 15:36:23 +0200

    First Vue app

---
+++ lessons/lesson5/exercise3/firstVueProject/src/components/ChildComponent.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  props: {
+    message: String,
+    counter: Number,
+    incrementCounter: Function,
+  },
+  methods: {
+    tryToMutateProps() {
+      // This will cause a Vue warning in development mode
+      this.message = 'Changed in child' // This will trigger a warning
+      this.counter++ // This will trigger a warning
+    },
+  },
+}
+</script>
+
+<template>
+  <div class="child">
+    <h2>Child Component</h2>
+    <p>Message from parent: {{ message }}</p>
+    <p>Counter: {{ counter }}</p>
+
+    <button @click="tryToMutateProps">Try to mutate props directly</button>
+    <button @click="incrementCounter">Call parent function</button>
+  </div>
+</template>
+
+<style scoped>
+h2 {
+  color: #42b983;
+}
+</style>
---
+++ lessons/lesson5/exercise3/firstVueProject/src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<script>
+import ParentComponent from '../components/ParentComponent.vue'
+import ChildComponent from '../components/ChildComponent.vue'
+</script>
+
+<template>
+  <div class="about">
+    <ParentComponent />
+    <ChildComponent />
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
